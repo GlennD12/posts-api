df886982af533fc5024aa599b96c4f8044a62a13
Glenn Dale Magbanua <Glenn Dale Magbanua>
2026-01-28 21:24:11 +0800

    Second Commit

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
+Route::get('/', function(){
+    return response()->json([
+        'message' => 'Hello, api!',
+        'status' => 'success'
+    ]);
+});
+
+Route::get('/login', [AuthController::class, 'login']);
 
 Route::get('/user', function (Request $request) {
     return $request->user();
